3b2808cca50be4f2d39442b979ee3b6b957279ac
Dorian LUPU <dorian.lupu@gmail.com>
2013-01-01 13:18:41 +0100

    temporary disable background jobs (all jobs are synchronous)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,3 +65,5 @@ AmvApi::Application.configure do
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
 end
+
+Delayed::Worker.delay_jobs = false  #background jobs are executed synchronously
